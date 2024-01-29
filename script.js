9d7477be2f8abce69f136c1adfc4b126d74aeee6
usman <usman.naeem12257@gmail.com>
2024-01-29 23:51:47 +0500

    image gallery

---
+++ script.js
@@ -0,0 +1,27 @@
+const ScrollContainer = document.querySelector(".Gallery")
+const backButton = document.querySelector(".fa-circle-chevron-left")
+const forwardButton = document.querySelector(".fa-circle-chevron-right")
+
+
+ScrollContainer.addEventListener("wheel",(e)=>{
+
+e.preventDefault()
+
+ScrollContainer.scrollLeft = ScrollContainer.scrollLeft + e.deltaY
+
+ScrollContainer.style.scrollBehavior = "auto"
+
+})
+
+backButton.addEventListener("click",(e)=>{
+    ScrollContainer.style.scrollBehavior = "smooth"
+    ScrollContainer.scrollLeft-=900
+    
+})
+
+forwardButton.addEventListener("click",(e)=>{
+    
+    ScrollContainer.style.scrollBehavior = "smooth"
+    
+    ScrollContainer.scrollLeft+=900
+})
